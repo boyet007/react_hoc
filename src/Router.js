3bb7e8caacf883be93d0162731066610c1aa68eb
Philip Loa <boyet007@gmail.com>
2019-09-06 13:53:32 +0700

    auth router

---
+++ src/Router.js
@@ -0,0 +1,41 @@
+import React, {Fragment} from 'react';
+import {Admin, User} from './Auth'
+
+import { 
+    BrowserRouter as Router, 
+    Route, 
+    Link
+} from "react-router-dom";
+
+const Navigation = () => (
+    <ul style={{display: "flex", justifyContent: "space-around" }}>
+        <li><Link to="/"> Home </Link></li>
+        <li><Link to="/admin"> Admin Page </Link></li>
+        <li><Link to="/user"> User Page </Link></li>
+    </ul>
+);
+
+const HomeComponent = () => (
+    <h1>Selamat datang</h1>
+);
+
+const AdminComponent = () => (
+    <h1>Hai Admin, Kamu cantik deh!</h1>
+);
+
+const UserComponent = () => (
+    <h1>Hai user, Jomblo kan!</h1>
+);
+
+const MainRouter = () => (
+    <Router>
+        <Fragment>
+            <Navigation/>
+            <Route path="/" component={HomeComponent} exact={true} />
+            <Route path="/admin" component={Admin(AdminComponent)} />
+            <Route path="/user" component={User(UserComponent)} />
+        </Fragment>
+    </Router>
+);
+
+export default MainRouter;
